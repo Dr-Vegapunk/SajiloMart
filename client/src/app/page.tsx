81f0c31eff9dfe1591f69fd2222d7bea8a3de6be
Dr-vegapunk <Ishankarki7624@gmail.com>
2025-04-03 08:45:24 +0545

    change: in page.tsx done some changes

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Home from './(user)/product/page'
+import { Header } from '@/components/header'
+import { Footer } from '@/components/footer'
 
 const LandingPage = () => {
   return (
     <div>
+      <Header />
       <Home />
+      <Footer />
     </div>
   )
 }
